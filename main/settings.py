3d71221e2a649d1a172e825dd110841be49f1105
mehmet-turkeri <mehmetturkeri51@gmail.com>
2022-10-27 17:15:09 +0200

    done

--- main/settings.py
+++ main/settings.py
@@ -34,12 +34,14 @@ INSTALLED_APPS = [
     'drf_yasg',
     'dj_rest_auth',
     'django_filters',
+    'corsheaders',
     # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -49,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'main.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
